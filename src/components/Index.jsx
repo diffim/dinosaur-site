59d8833814a1e932c92d16749686e8bab4188d4b
JeweIxx <vhiunese@gmail.com>
2022-12-22 23:59:16 +0400

    fixed img bug

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
-
+import trex from "/trex.png";
 function Index() {
   return (
     <div className="w-full min-h-screen overflow-hidden  h-full">
@@ -9,11 +9,7 @@ function Index() {
           <p className="text-2xl mt-20 to-blue-900">
             AWESOME SITE ABOUYT GUNZ AND DINOSAURS AND MONEY
           </p>
-          <img
-            src="https://o.remove.bg/downloads/6133bba4-669d-45df-bfff-4eea621c5346/030fefb2a5fb677d5d7f28e80bedd527-4153255451-removebg-preview.png"
-            alt="trex"
-            className="w-[70%] md:-mt-20"
-          />
+          <img src={trex} alt="trex" className="w-[70%] md:-mt-20" />
         </div>
       </div>
     </div>
